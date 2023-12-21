d10705f4abc62fc092125fcbce1a07a7907bfddf
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2023-12-21 20:02:17 +0100

    implemented two libraries - one for utilities, and other for settings

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
-from tkinter import *
+from tkinter  import *
+from settings import WIDTH         as window_width
+from settings import HEIGHT        as window_height
+from utils    import width_prct    as width_resize
+from utils    import height_prct   as height_resize
 
 
 root = Tk() # instantiating a window
@@ -7,7 +11,7 @@ root = Tk() # instantiating a window
 # Override the settings of the window
 
 root.title('MINESWEEPER') # providing a title for the game/window
-root.geometry('800x600') # drawing up a size of the window
+root.geometry(f'{window_width}x{window_height}') # drawing up a size of the window
 root.resizable(False, False) # to ensure that the size of the window cannot be resized
 root.configure(bg='blue') # set a background color for the window
 
@@ -15,16 +19,16 @@ root.configure(bg='blue') # set a background color for the window
 top_frame = Frame(
         root,
         bg='red', 
-        width = 760,
-        height = 50,
+        width = width_resize(95),
+        height = height_resize(8.333),
         )
 top_frame.place(x=20, y=20)
 
 left_frame = Frame(
         root,
         bg='green',
-        width = 190,
-        height = 510
+        width = width_resize(23.75),
+        height = height_resize(85)
         )
 left_frame.place(x = 20, y=70)
 
